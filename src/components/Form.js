32d809e5de1f6ee35bd71eed0488b8dd1f6122f1
PianoFF <Fangfei1star@gmail.com>
2020-01-08 13:30:19 +0000

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 class Form extends React.Component {
   state = {
-    firstName: "John",
-    lastName: "Henry"
+    firstName: "First Name",
+    lastName: "Last Name "
   }
 
   handleFirstNameChange = event => {
@@ -13,16 +13,35 @@ class Form extends React.Component {
   }
 
   handleLastNameChange = event => {
+    /* 👇to see event and event's attributes properties: 
+    event.persist()
+    console.log(event.target)*/
     this.setState({
       lastName: event.target.value
     })
   }
 
+
+  // handleSubmit = event => {
+  //   event.preventDefaul(); 
+  //   formData = {
+  //     firstName: this.state.firstName, 
+  //     lastName: this.state.lastName
+  //   }
+  //   this.doSomethingWithThisData(formData)
+  //   /* 👆 This function could be defined in the current component or passed down as a prop. 
+  //     currently, we don't have any server to send our data.,  
+  //   */
+  // } 
+
   render() {
     return (
-      <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
+      <form onSubmit={this.handleSubmit}>
+        <lable htmlFor='first-name'>First Name </lable>
+        <input type="text" id='first-name' name="firstName" onChange={this.handleFirstNameChange} value={this.state.firstName} />
+        <br></br>
+        <lable htmlFor='last-name'>Last Name </lable>               
+        <input type="text" id='last-name' name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
       </form>
     )
   }
